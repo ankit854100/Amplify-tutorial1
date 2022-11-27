4e957e5d8f2b5e089b415afb95d20c572beff3fe
unknown <ankit854100@gmail.com>
2022-11-27 16:48:41 +0530

    todo's functionality added without css

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifytutorial1": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
 import './App.css';
+import { API } from 'aws-amplify';
+import { listTodos } from './graphql/queries';
+import { createTodo, deleteTodo } from './graphql/mutations'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    fetchToDos();
+  }, [])
+
+  async function fetchToDos() {
+    let response = await API.graphql({
+      query: listTodos
+    });
+
+    setItems(response.data.listTodos.items);
+  }
+
+  async function createTodoFun() {
+    let myTodo = { name: name, description: description };
+    setName("");
+    setDescription("");
+
+    let response = await API.graphql({
+      query: createTodo,
+      variables: {
+        input: myTodo
+      }
+    });
+
+    fetchToDos();
+  }
+
+  async function deletetoDoFun(id){
+    const response = await API.graphql({
+      query: deleteTodo,
+      variables: {
+        input: { id }
+      }
+    });
+
+    setItems(prev => prev.filter(item => item.id !== id));
+    console.log(response);
+  }
+
   return (
     <div className="App">
-      Hello AWS Amplify!
-      <h3>Version 2</h3>
+      <input type='text' placeholder='name...' onChange={(e) => setName(e.target.value)} />
+      <input type='text' placeholder='description...' onChange={e => setDescription(e.target.value)} />
+      <button onClick={createTodoFun}>create todo</button>
+      {items.map(item => {
+        return (
+          <div key={item.id}>
+            <p>{item.name} : {item.description}</p>
+            <button onClick={() => deletetoDoFun(item.id)}>delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
